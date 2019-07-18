ac11caec1fa12bb9cc04871cc88ca8d4beee63a0
Darrell Meyer <darrellmeyer@gmail.com>
2019-07-18 12:07:53 -0400

    More samples

---
+++ chopsticks-sample/src/main/scala/dev/chopsticks/sample/app/AkkaStreamInterruptionSampleApp.scala
@@ -0,0 +1,36 @@
+package dev.chopsticks.sample.app
+
+import akka.stream.KillSwitches
+import akka.stream.scaladsl.{Keep, Sink, Source}
+import com.typesafe.config.Config
+import dev.chopsticks.fp.{AkkaApp, LogEnv, ZIOExt}
+import zio.{ZIO, ZManaged}
+
+import scala.concurrent.duration._
+
+object AkkaStreamInterruptionSampleApp extends AkkaApp {
+  type Env = AkkaApp.Env
+
+  protected def createEnv(untypedConfig: Config) = ZManaged.environment[AkkaApp.Env]
+
+  protected def run = {
+    val stream = ZIOExt.interruptableGraph(
+      ZIO.access[LogEnv] { env =>
+        Source(1 to 10)
+          .throttle(1, 1.second)
+          .wireTap(n => env.logger.info(s"Out: $n"))
+          .map { v =>
+            if (v > 4) {
+              throw new IllegalStateException("shit the bed")
+            }
+            v
+          }
+          .viaMat(KillSwitches.single)(Keep.right)
+          .toMat(Sink.fold(0)(_ + _))(Keep.both)
+      },
+      graceful = true
+    )
+
+    stream.unit
+  }
+}
---
+++ chopsticks-sample/src/main/scala/dev/chopsticks/sample/app/DstreamSampleApp.scala
@@ -0,0 +1,128 @@
+package dev.chopsticks.sample.app
+
+import akka.actor.ActorSystem
+import akka.grpc.GrpcClientSettings
+import akka.stream.KillSwitches
+import akka.stream.scaladsl.{Keep, Sink, Source}
+import com.typesafe.config.Config
+import dev.chopsticks.dstream.DstreamEnv.WorkResult
+import dev.chopsticks.dstream.Dstreams.DstreamServerConfig
+import dev.chopsticks.dstream.{DstreamEnv, Dstreams}
+import dev.chopsticks.fp.ZIOExt.Implicits._
+import dev.chopsticks.fp.{AkkaApp, AkkaEnv, ZIOExt}
+import dev.chopsticks.sample.app.proto.dstream_sample_app._
+import zio.clock.Clock
+import zio._
+
+import scala.concurrent.duration._
+
+object DstreamSampleApp extends AkkaApp {
+
+  type DsEnv = DstreamEnv[Assignment, Result]
+  type Env = AkkaApp.Env with DsEnv
+
+  protected def createEnv(untypedConfig: Config) = {
+    ZManaged.environment[AkkaApp.Env].map { env =>
+      new AkkaApp.LiveEnv with DsEnv {
+        implicit val actorSystem: ActorSystem = env.actorSystem
+        object dstreamService extends DstreamEnv.LiveService[Assignment, Result](rt) {
+          unsafeRunToFuture(updateQueueGauge.repeat(ZSchedule.fixed(1.second)).provide(env))
+            .onComplete(t => s"METRICS COMLETED ===================== $t")
+        }
+      }
+    }
+  }
+
+  private val runServer = {
+    val graphTask = ZIO.access[AkkaEnv with DsEnv with Clock] { implicit env =>
+      import env._
+
+      val ks = KillSwitches.shared("server shared killswitch")
+
+      Source(1 to Int.MaxValue)
+        .map(Assignment(_))
+        .via(ZIOExt.interruptableMapAsyncUnordered(12) { assignment: Assignment =>
+          Dstreams
+            .distribute(assignment) { result: WorkResult[Result] =>
+              Task.fromFuture { _ =>
+                result.source
+                  .via(ks.flow)
+                  .take(1)
+                  .runForeach { _ =>
+                    //                    println(s"Server < [worker=$workerId][assignment=${assignment.valueIn}] $r")
+                  }
+              }
+            }
+        })
+        .via(ks.flow)
+        //        .wireTap(a => println(s"Server < completed $a"))
+        .toMat(Sink.ignore)(Keep.right)
+        .mapMaterializedValue(f => (ks, f))
+    }
+
+    ZIOExt.interruptableGraph(graphTask, graceful = true)
+  }
+
+  protected def runWorker(client: DstreamSampleAppClient, id: Int) = {
+    Dstreams
+      .work(client.work().addHeader(Dstreams.WORKER_ID_HEADER, id.toString)) { a =>
+        UIO {
+          //          println(s"Client < [worker=$id][assignment=${a.valueIn}]")
+          Source
+            .single(1)
+            .map(v => Result(a.valueIn * 10 + v))
+          //            .throttle(1, 1.second)
+          //            .wireTap(r => println(s"Client > [worker=$id][assignment=${a.valueIn}] $r"))
+          //            .map { v =>
+          //              if (math.random() > 0.9) {
+          //                throw new IllegalStateException("test worker death")
+          //              }
+          //              vE
+          //            }
+        }
+      }
+  }
+
+  protected def run: ZIO[Env, Throwable, Unit] = {
+    val createService = ZIO.access[AkkaEnv with DsEnv] { env =>
+      import env._
+      DstreamSampleAppPowerApiHandler(Dstreams.handle(env, _, _))
+    }
+    val port = 9999
+    val managedServer =
+      Dstreams.createManagedServer(DstreamServerConfig(port = port, idleTimeout = 5.seconds), createService)
+    val managedClient = Dstreams.createManagedClient(ZIO.access[AkkaEnv] { env =>
+      import env._
+      DstreamSampleAppClient(
+        GrpcClientSettings
+          .connectToServiceAt("localhost", port)
+          .withTls(false)
+          .withChannelBuilderOverrides(
+            _.eventLoopGroup(new io.grpc.netty.shaded.io.netty.channel.nio.NioEventLoopGroup(2))
+              .executor(env.dispatcher)
+          )
+      )
+    })
+    val resources = managedServer zip managedClient
+
+    resources.use {
+      case (_, client) =>
+        val task = for {
+          s <- runServer
+            .log("server graph")
+            .fork
+          _ <- ZIO.forkAll_ {
+            (1 to 12).map { id =>
+              runWorker(client, id) /*.either*/
+              //                .withResultLogging(s"client $id", _.toString)
+              //                .timeoutFail(new TimeoutException("timed out"))(1.second)
+                .repeat(ZSchedule.forever)
+            }
+          }
+          _ <- s.join
+        } yield ()
+
+        task.interruptChildren
+    }
+  }
+}
