861c1803c1681947cb8b41fc0df9ef6a1d53a487
Jamie Avins <jamie@avins.org>
2019-11-05 20:42:35 -0500

    Remove old kvdb module

--- chopsticks-kvdb/src/main/scala/dev/chopsticks/kvdb/codec/DbValueEncoder.scala
+++
@@ -1,5 +0,0 @@
-package dev.chopsticks.kvdb.codec
-
-trait DbValueEncoder[T] {
-  def encode(value: T): Array[Byte]
-}
--- chopsticks-kvdb/src/main/scala/dev/chopsticks/kvdb/codec/UnusedImplicits.scala
+++
@@ -1,7 +0,0 @@
-package dev.chopsticks.kvdb.codec
-
-object UnusedImplicits {
-  implicit final class UnusedOps[A](private val a: A) extends AnyVal {
-    def unused(): Unit = ()
-  }
-}
