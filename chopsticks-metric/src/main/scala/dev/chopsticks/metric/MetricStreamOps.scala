6c16f320afdac3e7618d4776262301a4eca7d7b6
Pawel Iwanow <pwliwanow@gmail.com>
2020-06-15 23:54:21 -0400

    Add AkkaStreamSourceMetricOps

--- chopsticks-metric/src/main/scala/dev/chopsticks/metric/MetricStreamOps.scala
+++ chopsticks-metric/src/main/scala/dev/chopsticks/metric/MetricStreamOps.scala
@@ -1,8 +1,34 @@
 package dev.chopsticks.metric
 
-import akka.stream.scaladsl.Flow
+import akka.stream.scaladsl.{Flow, Source}
 
 object MetricStreamOps {
+  implicit class AkkaStreamSourceMetricOps[+Elem, +Mat](source: Source[Elem, Mat]) {
+    def metricCounter(metric: MetricCounter): Source[Elem, Mat] = {
+      source
+        .map { v =>
+          metric.inc(1.0)
+          v
+        }
+    }
+
+    def metricCounter[N](metric: MetricCounter, toNumeric: Elem => N)(implicit num: Numeric[N]): Source[Elem, Mat] = {
+      source
+        .map { v =>
+          metric.inc(num.toDouble(toNumeric(v)))
+          v
+        }
+    }
+
+    def metricGauge[N](metric: MetricGauge, toValue: Elem => N)(implicit num: Numeric[N]): Source[Elem, Mat] = {
+      source
+        .map { v =>
+          metric.set(num.toDouble(toValue(v)))
+          v
+        }
+    }
+  }
+
   implicit class AkkaStreamFlowMetricOps[-In, +Out, +Mat](flow: Flow[In, Out, Mat]) {
     def metricCounter(metric: MetricCounter): Flow[In, Out, Mat] = {
       flow
