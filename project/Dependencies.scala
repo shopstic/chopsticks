56a0d0b4d5cd246c86b732b28f99db2ce1488c1b
Darrell Meyer <darrellmeyer@gmail.com>
2020-06-22 18:09:56 -0400

    Revert back to ZIO RC20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val SCALA_VERSION = "2.13.2"
   val AKKA_VERSION = "2.6.6"
   val AKKA_HTTP_VERSION = "10.1.10"
-  val ZIO_VERSION = "1.0.0-RC21"
+  val ZIO_VERSION = "1.0.0-RC20"
 //  val ZIO_VERSION = "1.0.0-RC18"
   val IZUMI_VERSION = "0.10.14"
 
