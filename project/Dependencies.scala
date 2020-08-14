e47fea8c5e0f759b1a7eafcc4696c9fc5804023a
Scala Steward <me@scala-steward.org>
2020-08-14 18:44:24 +0200

    Update distage-core, ... to 0.10.17

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val AKKA_VERSION = "2.6.8"
   val AKKA_HTTP_VERSION = "10.1.10"
   val ZIO_VERSION = "1.0.0"
-  val IZUMI_VERSION = "0.10.16"
+  val IZUMI_VERSION = "0.10.17"
   val REFINED_VERSION = "0.9.15"
   val CALIBAN_VERSION = "0.9.0"
 
