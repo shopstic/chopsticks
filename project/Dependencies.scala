8f28bd2884bdf858e5ce172ad3de0083ec345708
Scala Steward <me@scala-steward.org>
2020-08-22 08:26:26 +0200

    Update scalatest to 3.2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -90,7 +90,7 @@ object Dependencies {
 
   val scalatestDeps = Seq(
     "org.scalactic" %% "scalactic" % "3.2.1",
-    "org.scalatest" %% "scalatest" % "3.2.1",
+    "org.scalatest" %% "scalatest" % "3.2.2",
     "org.scalatestplus" %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
     "org.scalacheck" %% "scalacheck" % "1.14.3",
     "org.scalamock" %% "scalamock" % "5.0.0"
