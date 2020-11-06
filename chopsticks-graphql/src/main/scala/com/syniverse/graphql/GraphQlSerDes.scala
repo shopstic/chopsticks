6803570b70d844b4cc9473b619be04bcdf4ec391
Jacky Nguyen <nktpro@gmail.com>
2020-11-06 11:50:30 -0500

    Fix non exhaustive match

--- chopsticks-graphql/src/main/scala/com/syniverse/graphql/GraphQlSerDes.scala
+++ chopsticks-graphql/src/main/scala/com/syniverse/graphql/GraphQlSerDes.scala
@@ -28,6 +28,7 @@ object GraphQlSerDes {
       case x: SelectionBuilder.Mapping[_, Any, A] @unchecked => x.fromGraphQL(value)
       case x: SelectionBuilder.Concat[_, Any, Any] @unchecked =>
         x.fromGraphQL(value).asInstanceOf[Either[DecodingError, A]]
+      case SelectionBuilder.Pure(a) => Right(a)
     }
   }
 
