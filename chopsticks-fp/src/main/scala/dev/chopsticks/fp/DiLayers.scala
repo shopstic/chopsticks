8604ae652df1f233c73f63ae1c11cd3504ef7a4f
Jacky Nguyen <nktpro@gmail.com>
2020-11-10 15:16:54 -0500

    Add support for ZManaged to DiLayers

--- chopsticks-fp/src/main/scala/dev/chopsticks/fp/DiLayers.scala
+++ chopsticks-fp/src/main/scala/dev/chopsticks/fp/DiLayers.scala
@@ -2,7 +2,7 @@ package dev.chopsticks.fp
 
 import distage.{HasConstructor, ModuleDef, Tag}
 import izumi.distage.model.definition.Module
-import zio.{Has, RLayer, ZIO}
+import zio.{Has, RLayer, RManaged, ZIO}
 
 object DiLayers {
   def apply(layers: LayerBinding*): Module = layers.foldLeft(Module.empty)(_ ++ _.layerModule)
@@ -29,6 +29,12 @@ object DiLayers {
     implicit def fromLayer[R: HasConstructor, A: Tag](layer: RLayer[R, Has[A]]): LayerBinding =
       LayerBinding(new ModuleDef {
         make[A].fromHas(layer = layer)
+      })
+
+    @SuppressWarnings(Array("org.wartremover.warts.NonUnitStatements"))
+    implicit def fromManaged[R: HasConstructor, A: Tag](managed: RManaged[R, A]): LayerBinding =
+      LayerBinding(new ModuleDef {
+        make[A].fromHas(resource = managed)
       })
 
     @SuppressWarnings(Array("org.wartremover.warts.NonUnitStatements"))
