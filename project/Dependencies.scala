355d9e43bd192fc3728c565223f77d46b9c446e9
Scala Steward <me@scala-steward.org>
2020-12-05 18:48:32 +0100

    Update kittens to 2.2.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -64,7 +64,7 @@ object Dependencies {
   )
 
   val kittensDeps = Seq(
-    "org.typelevel" %% "kittens" % "2.2.0"
+    "org.typelevel" %% "kittens" % "2.2.1"
   )
 
   val pureconfigDeps = Seq("pureconfig", "pureconfig-akka")
