7457a70c4218f20c31553f42e611b2dcbfcfd606
Jacky Nguyen <nktpro@gmail.com>
2020-12-22 06:17:38 -0500

    Implement LoggedRace

---
+++ chopsticks-fp/src/main/scala/dev/chopsticks/fp/util/LoggedRace.scala
@@ -0,0 +1,33 @@
+package dev.chopsticks.fp.util
+
+import dev.chopsticks.fp.zio_ext.{MeasuredLogging, ZIOExtensions}
+import zio.{RIO, Task, ZIO}
+
+import scala.collection.immutable.Queue
+
+final class LoggedRace[-R] private (queue: Queue[R with MeasuredLogging => Task[Unit]]) {
+  def add[R1 <: R](name: String, task: RIO[R1, Unit]): LoggedRace[R1] = {
+    new LoggedRace[R1](queue.enqueue((env: R1 with MeasuredLogging) => task.log(name).provide(env)))
+  }
+
+  def run(): RIO[R with MeasuredLogging, Unit] = {
+    ZIO
+      .environment[R with MeasuredLogging]
+      .flatMap { env =>
+        val tasks = queue.toList.map(_(env))
+
+        tasks match {
+          case head :: tail :: Nil =>
+            head.raceFirst(tail)
+          case head :: tail =>
+            head.run.raceAll(tail.map(_.run)).flatMap(ZIO.done(_)).refailWithTrace
+          case Nil =>
+            Task.unit
+        }
+      }
+  }
+}
+
+object LoggedRace {
+  def apply() = new LoggedRace[Any](Queue.empty)
+}
