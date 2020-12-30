034e7b21f3fdaadc1313ae2111f09011f5a39f09
Scala Steward <me@scala-steward.org>
2020-12-30 14:29:48 +0100

    Update akka-grpc-runtime, ... to 1.0.2-60-b9c1f1e6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -56,7 +56,7 @@ object Dependencies {
   )
 
   val akkaGrpcRuntimeDeps = Seq(
-    "com.lightbend.akka.grpc" %% "akka-grpc-runtime" % "1.0.2-57-c8612a00"
+    "com.lightbend.akka.grpc" %% "akka-grpc-runtime" % "1.0.2-60-b9c1f1e6"
   )
 
   val catsCoreDeps = Seq(
