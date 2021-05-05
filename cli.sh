49dc120c25ce39480bf6cfe7a0f05f416ecc522b
Jacky Nguyen <nktpro@gmail.com>
2021-05-05 11:18:04 -0400

    Move fdb to service container

--- cli.sh
+++ cli.sh
@@ -33,12 +33,12 @@ ci_run_in_shell() {
 ci_run() {
   local GITHUB_REF=${GITHUB_REF:?"GITHUB_REF env variable is required"}
 
+  mkdir -p /etc/foundationdb/
   export FDB_CLUSTER_FILE=/etc/foundationdb/fdb.cluster
-  service foundationdb start
+  echo "docker:docker@127.0.0.1:4500" > "${FDB_CLUSTER_FILE}"
 
   export SBT_OPTS="-server -XX:+UseG1GC -Xms6g -Xmx6g -Xss6m"
   ./cli.sh build
-  service foundationdb stop
 
   if [[ "${GITHUB_REF}" == "refs/heads/master" ]]; then
     local PUBLISH_VERSION
