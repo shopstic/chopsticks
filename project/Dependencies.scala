54311e8c98ff1639fc743a372f28b1f66fc67f48
Scala Steward <me@scala-steward.org>
2021-12-16 04:24:27 +0100

    Update caliban, caliban-akka-http, ... to 1.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val ZIO_VERSION = "1.0.12"
   val IZUMI_VERSION = "1.0.8"
   val REFINED_VERSION = "0.9.28"
-  val CALIBAN_VERSION = "1.2.4"
+  val CALIBAN_VERSION = "1.3.1"
 
   val akkaSlf4jDeps = Seq(
     "com.typesafe.akka" %% "akka-slf4j" % AKKA_VERSION
