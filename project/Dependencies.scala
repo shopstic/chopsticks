c860a852f746393642bfb673025e85374d04a3e9
Scala Steward <me@scala-steward.org>
2022-01-13 20:35:44 +0100

    Update scala-library to 2.13.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 //noinspection ScalaUnusedSymbol,TypeAnnotation
 object Dependencies {
-  val SCALA_VERSION = "2.13.7"
+  val SCALA_VERSION = "2.13.8"
   val AKKA_VERSION = "2.6.18"
   val AKKA_HTTP_VERSION = "10.2.1"
   val ZIO_VERSION = "1.0.13"
