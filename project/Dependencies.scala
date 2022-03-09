2b2a2bb9e516b595433f054642e5194dcd75f166
Scala Steward <me@scala-steward.org>
2022-03-09 10:39:35 +0100

    Update fdb-java to 6.3.24

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -116,7 +116,7 @@ object Dependencies {
   )
 
   val fdbDeps = Seq(
-    "org.foundationdb" % "fdb-java" % "6.3.23"
+    "org.foundationdb" % "fdb-java" % "6.3.24"
   )
 
   val shapelessDeps = Seq(
