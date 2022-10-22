003359c6d2b8c9be0e68a4ae60fecd7373e85355
Jacky Nguyen <nktpro@gmail.com>
2022-10-22 13:22:26 -0400

    Add refinedStringAsMapKeyConfigReader

--- chopsticks-util/src/main/scala/dev/chopsticks/util/config/PureconfigConverters.scala
+++ chopsticks-util/src/main/scala/dev/chopsticks/util/config/PureconfigConverters.scala
@@ -112,4 +112,15 @@ object PureconfigConverters {
 
   implicit def exportReader[A]: Exported[ConfigReader[A]] = macro ExportMacros.exportDerivedReader[A]
   implicit def exportWriter[A]: Exported[ConfigWriter[A]] = macro ExportMacros.exportDerivedWriter[A]
+
+  implicit def refinedStringAsMapKeyConfigReader[F[_, _], P, V](implicit
+    refType: RefType[F],
+    validate: Validate[String, P],
+    typeTag: WeakTypeTag[F[String, P]]
+  ): ConfigReader[Map[F[String, P], V]] = {
+    pureconfig.configurable.genericMapReader { s =>
+      import cats.syntax.either._
+      refType.refine[P](s).leftMap(CannotConvert(s, typeTag.tpe.toString, _))
+    }
+  }
 }
