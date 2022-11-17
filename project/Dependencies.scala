8bd3b64139fdb29e0496bf6e5e7adc1ab77f1661
Scala Steward <scala_steward@virtuslab.com>
2022-11-17 03:23:37 +0000

    Update janino to 3.1.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -95,7 +95,7 @@ object Dependencies {
   )
 
   val janinoDeps = Seq(
-    "org.codehaus.janino" % "janino" % "3.1.8"
+    "org.codehaus.janino" % "janino" % "3.1.9"
   )
 
   val scalatestDeps = Seq(
