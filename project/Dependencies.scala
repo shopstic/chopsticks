4b5320d942436b055b7bf7d0a55a2f87e32b712c
Scala Steward <scala_steward@virtuslab.com>
2023-02-10 22:23:02 +0000

    Update circe-core, circe-generic, ... to 0.14.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   val ZIO_VERSION = "1.0.14"
   val IZUMI_VERSION = "1.0.10"
   val REFINED_VERSION = "0.9.29"
-  val CIRCE_VERSION = "0.14.3"
+  val CIRCE_VERSION = "0.14.4"
   val CALIBAN_VERSION = "1.3.3"
 
   val akkaSlf4jDeps = Seq(
