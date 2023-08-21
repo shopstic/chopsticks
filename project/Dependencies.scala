7d5e58e623a5e0d9b57b108f5ac6909deba39728
Scala Steward <scala_steward@virtuslab.com>
2023-08-21 18:09:54 +0000

    Update zio, zio-streams, zio-test, ... to 2.0.16

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   // The last Apache 2.0 licensed Akka
   val AKKA_VERSION = "2.6.20"
   val AKKA_HTTP_VERSION = "10.2.10"
-  val ZIO_VERSION = "1.0.18"
+  val ZIO_VERSION = "2.0.16"
   val IZUMI_VERSION = "1.0.10"
   val REFINED_VERSION = "0.9.29"
   val CIRCE_VERSION = "0.14.3"
