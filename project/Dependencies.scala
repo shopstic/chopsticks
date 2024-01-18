1418c4504b6523f8c3991934be33a9fcb73a5fd0
Scala Steward <scala_steward@virtuslab.com>
2024-01-18 20:18:37 +0000

    Update logstage-adapter-slf4j, ... to 1.2.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val AKKA_VERSION = "2.6.20"
   val AKKA_HTTP_VERSION = "10.2.10"
   val ZIO_VERSION = "1.0.18"
-  val IZUMI_VERSION = "1.0.10"
+  val IZUMI_VERSION = "1.2.4"
   val REFINED_VERSION = "0.9.29"
   val CIRCE_VERSION = "0.14.3"
   val CALIBAN_VERSION = "1.3.3"
