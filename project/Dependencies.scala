0946ea50ae5dfe472431d3448ef0d4105a4e748b
Scala Steward <scala_steward@virtuslab.com>
2025-01-21 01:08:42 +0000

    Update cats-core to 2.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -70,7 +70,7 @@ object Dependencies {
   )
 
   val catsCoreDeps = Seq(
-    "org.typelevel" %% "cats-core" % "2.8.0"
+    "org.typelevel" %% "cats-core" % "2.13.0"
   )
 
   val pureconfigDeps = Seq("pureconfig", "pureconfig-pekko")
